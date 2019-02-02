f6cae42634f5ce07e5b6dc5130bd54a80c2d6a38
Dmitry Markov <forall@stylive.com>
2019-02-02 12:49:19 +0100

    Set up Cucumber boilerplate

---
+++ spec/cucumber/steps/index.js
@@ -0,0 +1,25 @@
+import { When, Then } from 'cucumber'
+
+When('the client creates a POST request to /users', (cb) => {
+  cb(null, 'pending')
+})
+
+When('attaches a generic empty payload', (cb) => {
+  cb(null, 'pending')
+})
+
+When('sends the request', (cb) => {
+  cb(null, 'pending')
+})
+
+Then('our API should respond with a 400 HTTP status code', (cb) => {
+  cb(null, 'pending')
+})
+
+Then('the payload of the response should be a JSON object', (cb) => {
+  cb(null, 'pending')
+})
+
+Then('contains a message property which says "Payload should not be empty"', (cb) => {
+  cb(null, 'pending')
+})
