918b45ff59e077c5cd79e37996eee4974a65c31b
Dmitry Markov <forall@stylive.com>
2019-02-02 10:12:55 +0100

    Project boilerplate

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import '@babel/polyfill'
+import http from 'http'
+
+const requestHandler = function (req, res) {
+  res.writeHead(200, { 'Content-Type': 'text/plain' })
+  res.end('Simplest HTTP server!')
+}
+const server = http.createServer(requestHandler)
+server.listen(8080)
