cf6daa803da3565e0473062ee6c8913bf0b7494d
Dmitry Markov <forall@stylive.com>
2019-02-02 15:20:29 +0100

    manually set Assertion Errors

--- spec/cucumber/steps/index.js
+++ spec/cucumber/steps/index.js
@@ -1,25 +1,50 @@
+import { AssertionError } from 'assert'
+import superagent from 'superagent'
 import { When, Then } from 'cucumber'
 
-When('the client creates a POST request to /users', (cb) => {
-  cb(null, 'pending')
+When('the client creates a POST request to /users', function () {
+  this.request = superagent('POST', 'localhost:8080/users')
 })
 
-When('attaches a generic empty payload', (cb) => {
-  cb(null, 'pending')
+When('attaches a generic empty payload', () => {
+  return undefined
 })
 
-When('sends the request', (cb) => {
-  cb(null, 'pending')
+When('sends the request', function (cb) {
+  this.request
+    .then((response) => {
+      this.response = response.res
+      cb()
+    })
+    .catch((err) => {
+      this.response = err.response
+      cb()
+    })
 })
 
-Then('our API should respond with a 400 HTTP status code', (cb) => {
-  cb(null, 'pending')
+Then('our API should respond with a 400 HTTP status code', function () {
+  if (this.response.statusCode !== 400) {
+    throw new AssertionError({
+      expected: 400,
+      actual: this.response.statusCode
+    })
+  }
 })
 
-Then('the payload of the response should be a JSON object', (cb) => {
-  cb(null, 'pending')
+Then('the payload of the response should be a JSON object', function () {
+  const contentType = this.response.headers['Content-Type'] || this.response.headers['content-type']
+  if (!contentType || !contentType.includes('application/json')) {
+    throw new Error('Response not of Content-Type application/josn')
+  }
+  try {
+    this.responsePayload = JSON.parse(this.response.text)
+  } catch (e) {
+    throw new Error('Response not a valid JSON object.')
+  }
 })
 
-Then('contains a message property which says "Payload should not be empty"', (cb) => {
-  cb(null, 'pending')
+Then('contains a message property which says "Payload should not be empty"', function () {
+  if (this.responsePayload.message !== 'Payload should not be empty') {
+    throw new Error()
+  }
 })
