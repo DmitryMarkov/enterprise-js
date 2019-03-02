50f02c9d3ccbb5a09df8704bb09f902b24c1af79
Dmitry Markov <forall@stylive.com>
2019-03-02 18:46:20 +0100

    Add handle non-JSON and malformed requests

--- spec/cucumber/steps/index.js
+++ spec/cucumber/steps/index.js
@@ -5,33 +5,38 @@ import { When, Then } from 'cucumber'
 
 const env = config().parsed
 
-When('the client creates a POST request to /users', function () {
-  // console.log(env)
-  this.request = superagent('POST', `${env.SERVER_HOSTNAME}:${env.SERVER_PORT}/users`)
+When(/^the client creates a POST request to \/users$/, function() {
+  // console.log(process.env)
+  this.request = superagent(
+    'POST',
+    `${env.SERVER_HOSTNAME}:${env.SERVER_PORT}/users`
+  )
 })
 
-When('attaches a generic empty payload', () => {
+When(/^attaches a generic empty payload$/, () => {
   return undefined
 })
 
-When('sends the request', function (cb) {
+When(/^sends the request$/, function(cb) {
   this.request
-    .then((response) => {
+    .then(response => {
       this.response = response.res
       cb()
     })
-    .catch((err) => {
+    .catch(err => {
       this.response = err.response
       cb()
     })
 })
 
-Then('our API should respond with a 400 HTTP status code', function () {
+Then(/^our API should respond with a 400 HTTP status code$/, function() {
   assert.strict.equal(this.response.statusCode, 400)
 })
 
-Then('the payload of the response should be a JSON object', function () {
-  const contentType = this.response.headers['Content-Type'] || this.response.headers['content-type']
+Then(/^the payload of the response should be a JSON object$/, function() {
+  const contentType =
+    this.response.headers['Content-Type'] ||
+    this.response.headers['content-type']
   if (!contentType || !contentType.includes('application/json')) {
     throw new Error('Response not of Content-Type application/josn')
   }
@@ -42,6 +47,48 @@ Then('the payload of the response should be a JSON object', function () {
   }
 })
 
-Then('contains a message property which says "Payload should not be empty"', function () {
-  assert.strict.equal(this.responsePayload.message, 'Payload should not be empty')
+Then(
+  /^contains a message property which says "Payload should not be empty"$/,
+  function() {
+    assert.strict.equal(
+      this.responsePayload.message,
+      'Payload should not be empty'
+    )
+  }
+)
+
+When(/^attaches a generic non-JSON payload$/, function() {
+  this.request.send(
+    '<?xml version="1.0 encoding="UTF-8" ?><email>test@test.com</email>'
+  )
+  this.request.set('Content-Type', 'text/xml')
 })
+
+When(/^attaches a generic malformed payload$/, function() {
+  this.request.send('{ "email": "test@test.com", name: }')
+  this.request.set('Content-Type', 'application/json')
+})
+
+Then(/^our API should respond with a 415 HTTP status code$/, function() {
+  assert.strict.equal(this.response.statusCode, 415)
+})
+
+Then(
+  /^contains a message property which says 'The "Content-Type" header must always be "application\/json"'$/,
+  function() {
+    assert.strict.equal(
+      this.responsePayload.message,
+      'The "Content-Type" header must always be "application/json"'
+    )
+  }
+)
+
+Then(
+  /^contains a message property which says "Payload should be in JSON format"$/,
+  function() {
+    assert.strict.equal(
+      this.responsePayload.message,
+      'Payload should be in JSON format'
+    )
+  }
+)
--- src/index.js
+++ src/index.js
@@ -3,19 +3,48 @@ import http from 'http'
 
 const requestHandler = function(req, res) {
   if (req.method === 'POST' && req.url === '/users') {
-    res.writeHead(400, {
-      'Content-Type': 'application/json',
+    const payloadData = []
+    req.on('data', data => {
+      payloadData.push(data)
     })
-    res.end(
-      JSON.stringify({
-        message: 'Payload should not be empty',
-      })
-    )
-    return
-  }
 
-  res.writeHead(200, { 'Content-Type': 'text/plain' })
-  res.end('Simplest HTTP server!')
+    req.on('end', () => {
+      if (payloadData.length === 0) {
+        res.writeHead(400, { 'Content-Type': 'application/json' })
+        res.end(
+          JSON.stringify({
+            message: 'Payload should not be empty',
+          })
+        )
+        return
+      }
+      if (req.headers['content-type'] !== 'application/json') {
+        res.writeHead(415, { 'Content-Type': 'application/json' })
+        res.end(
+          JSON.stringify({
+            message:
+              'The "Content-Type" header must always be "application/json"',
+          })
+        )
+        return
+      }
+      try {
+        const bodyString = Buffer.concat(payloadData).toString()
+        JSON.parse(bodyString)
+      } catch (e) {
+        res.writeHead(400, { 'Content-Type': 'application/json' })
+
+        res.end(
+          JSON.stringify({
+            message: 'Payload should be in JSON format',
+          })
+        )
+      }
+    })
+  } else {
+    res.writeHead(200, { 'Content-Type': 'text/plain' })
+    res.end('Simplest HTTP server!')
+  }
 }
 const server = http.createServer(requestHandler)
 server.listen(process.env.SERVER_PORT)
