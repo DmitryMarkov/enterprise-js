c932e8cd29a1e55a553415fe508802eb85159850
Dmitry Markov <forall@stylive.com>
2019-03-13 09:18:44 +0100

    split middleware to each file

--- src/middleware/index.js
+++ src/middleware/index.js
@@ -1,43 +1,5 @@
-function checkEmptyPayload(req, res, next) {
-  if (
-    ['POST', 'PATCH', 'PUT'].includes(req.method) &&
-    req.headers['content-length'] === '0'
-  ) {
-    res.status(400)
-    res.set('Content-Type', 'application/json')
-    res.json({
-      message: 'Payload should not be empty',
-    })
-  }
-  next()
-}
-
-function checkContentTypeIsSet(req, res, next) {
-  if (
-    req.headers['content-length'] &&
-    req.headers['content-length'] !== '0' &&
-    !req.headers['content-type']
-  ) {
-    res.status(400)
-    res.set('Content-Type', 'application/json')
-    res.json({
-      message:
-        'The "Content-Type" header must be set for requests with a non-empty payload',
-    })
-  }
-  next()
-}
-
-function checkContentTypeIsJson(req, res, next) {
-  if (!req.headers['content-type'].includes('application/json')) {
-    res.status(415)
-    res.set('Content-Type', 'application/json')
-    res.json({
-      message:
-        'The "Content-Type" header must always be "application/json"',
-    })
-  }
-  next()
-}
+import checkEmptyPayload from './checkEmptyPayload'
+import checkContentTypeIsSet from './checkContentTypeIsSet'
+import checkContentTypeIsJson from './checkContentTypeIsJson'
 
 export { checkContentTypeIsJson, checkContentTypeIsSet, checkEmptyPayload }
